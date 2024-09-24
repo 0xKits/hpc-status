cfae6bc73ee13c6f8cf9649246b117cb9c6b0124
0xKits <kitsgit@gmail.com>
2024-09-24 22:05:48 +0530

    endpt api done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+"use client"
 
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [endpoint, setEndpoint] = useState<string>("")
+
+  useEffect(() => {
+    async function updateEndpoint() {
+      const response = await fetch("/api/status");
+      const data = await response.json();
+
+      console.log(data)
+
+      setEndpoint(data.endpoint)
+    }
+    updateEndpoint()
+  }, [])
+
   return (
     <div>
-      Hello
+      {endpoint}
     </div>
   );
 }
